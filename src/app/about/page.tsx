c1378a54214cb168d632e3e0075b5979a7001e16
@designervenkat <murty.av@gmail.com>
2025-08-04 15:36:27 +0000

    basics - 00

---
+++ src/app/about/page.tsx
@@ -0,0 +1,3 @@
+export default function AboutUs() {
+    return <h2 className="text-xl font-medium">This is about page</h2>
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,3 @@
+export default function Contact() {
+    return <h2 className="text-xl font-medium">This is contact page</h2>
+}
